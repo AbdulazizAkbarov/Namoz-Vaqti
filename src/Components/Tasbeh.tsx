4e4116fe5f42568ebbcdfc40e69f4197007bd8e6
abdulaziz <abdulazizakbarov065@gmail.com>
2025-04-25 16:35:02 +0500

    a

--- src/Components/Tasbeh.tsx
+++ src/Components/Tasbeh.tsx
@@ -1,13 +1,30 @@
 
 import Ong from '../assets/ong'
 import { Link } from 'react-router'
+import touch from"../assets/touch-screen.png"
+import { useState } from 'react'
 
 function Tasbeh() {
+    const [count,setCount]=useState(0)
   return (
-    <div className='tasbeh'>
+    <div>
         <Link to={"/"}>
         <p className='text-2xl  text-white pt-2'><Ong/></p>
         </Link>
+
+
+         <div className=' mt-20 h-[100px] bg-[#00254783] rounded-xl text-center w-[96%] mx-auto'>
+            <p className='text-white text-5xl font-bold pt-6'>{count}</p>
+        </div>   
+
+
+        <div className=' border-1 border-[#002547] transition ease-in active:bg-[#00254780] active:border-1 acttive:border-transparent p-6 w-[100px] h-[100px] rounded-full mt-[200px] mx-auto'
+        onClick={()=>{
+            setCount(count+1)
+        }}
+        >
+            <img className='w-[10] h-[10]' src={touch} alt="" />
+        </div>
     </div>
   )
 }
